a725142fb9d64d17218b397c1743a1edd30d651e
Vladimir <worobev2001@gmail.com>
2020-09-13 02:05:31 +0300

    Created rotation method

--- main.cpp
+++ main.cpp
@@ -1,8 +1,50 @@
 #include <iostream>
 #include "model_t.h"
+#include "model_f.h"
 
 
 int main()
 {
-  
+	Tetranom o{ {{'1', '1'},
+			 {'1', '1'}},
+			 '1' };
+	Tetranom i{ {{'0', '0', '0', '0', '0'},
+				 {'0', '0', '2', '0', '0'},
+				 {'0', '0', '2', '0', '0'},
+				 {'0', '0', '2', '0', '0'},
+				 {'0', '0', '2', '0', '0'}},
+				 '2' };
+	Tetranom t{ {{'0','3','0'},
+				 {'3', '3', '3'},
+				{'0','0','0'}},
+				 '3' };
+
+	/*Field field;
+	field.show_field();*/
+	t.show_tetranom();
+	t.rotation();
+	std::cout << std::endl;
+	t.show_tetranom();
+
+	std::cout << std::endl;
+	t.rotation();
+	std::cout << std::endl;
+	t.show_tetranom();
+
+	std::cout << std::endl;
+	t.rotation();
+	std::cout << std::endl;
+	t.show_tetranom();
+
+	std::cout << std::endl;
+	t.rotation();
+	std::cout << std::endl;
+	t.show_tetranom();
+
+	std::cout << std::endl;
+	t.rotation();
+	std::cout << std::endl;
+	t.show_tetranom();
+	/*field.search_full_rows();
+	field.show_field();*/
 }
