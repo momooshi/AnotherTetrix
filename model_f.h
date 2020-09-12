e3f69dd081a2a42da9024b4597e4346e85d2d55c
Vladimir <worobev2001@gmail.com>
2020-09-12 20:01:06 +0300

    Writed method show_field, search_full_rows, clean_row.

--- model_f.h
+++ model_f.h
@@ -1,19 +1,19 @@
 #ifndef MODEL_F
 #define MODEL_F
-#include <vector>
 
 class Field
 {
 private:
-	std::vector <std::vector <char>> Matrix;
+	short int field [20][10];
 public:
 	Field();
-	void clean_row();
+	void clean_row(int row_pointer);
 	void create_figure();
 	bool collisions();
 	void move_coord();
-	int get_width() { return m_width; }
-
+	int get_width();
+	void show_field();
+	bool search_full_rows();
 };
 
 #endif
